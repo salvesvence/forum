d94f958ae3be76d82d7d042d28ce129ae06aa736
salvesvence <salvesvence@gmail.com>
2018-03-03 00:05:01 +0100

    Now an unauthenticated user may not participate in a forum thread.

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -10,6 +10,17 @@ class ParticipateInForumTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    public function an_unauthenticated_user_may_not_participate_in_forum_threads()
+    {
+        $thread = factory('App\Thread')->create();
+        $reply = factory('App\Reply')->create();
+
+        $this->post("threads/$thread->id/replies", $reply->toArray());
+
+        $this->get($thread->path())->assertSee($reply->body);
+    }
+
+    /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
         $this->be($user = factory('App\User')->create());
