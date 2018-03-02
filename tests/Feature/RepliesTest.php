b5375b14ad55e0ecd262d27c35e413d58bb03eae
salvesvence <salvesvence@gmail.com>
2018-03-02 21:48:03 +0100

    Created the RepliesTest class.

---
+++ tests/Feature/RepliesTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RepliesTest extends TestCase
+{
+    use DatabaseMigrations;
+}
