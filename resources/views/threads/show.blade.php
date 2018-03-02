a294654c44d427cd7972ec1eee563924c21de4a5
salvesvence <salvesvence@gmail.com>
2018-03-02 22:27:16 +0100

    Created the threads/reply partial view.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,12 +13,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 @foreach($thread->replies as $reply)
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            {{ $reply->owner->name }} said {{ $reply->created_at->diffForHumans() }}
-                        </div>
-                        <div class="panel-body">{{ $reply->body }}</div>
-                    </div>
+                    @include('threads.reply')
                 @endforeach
             </div>
         </div>
