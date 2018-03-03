3808d4e0be2484f98dfa3d9a20c447c1f20d32d7
salvesvence <salvesvence@gmail.com>
2018-03-03 02:02:24 +0100

    Added the auth middleware to the store() function of ThreadsController class.

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -8,6 +8,14 @@ use Illuminate\Http\Request;
 class ThreadsController extends Controller
 {
     /**
+     * ThreadsController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->only('store');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
