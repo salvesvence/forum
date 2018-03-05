be6ba5202f1a75e6083184ee5dac585910edc64b
salvesvence <salvesvence@gmail.com>
2018-03-05 16:49:57 +0100

    Created the signIn() function into the TestCase class.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,19 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Sign a given user in test environment.
+     *
+     * @param null $user
+     * @return $this
+     */
+    protected function signIn($user = null)
+    {
+        $user = $user ?: create('App\User');
+
+        $this->actingAs($user);
+
+        return $this;
+    }
 }
