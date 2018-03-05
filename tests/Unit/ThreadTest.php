f541da3f1df9578ecd4b06d229fe23cb0e146837
salvesvence <salvesvence@gmail.com>
2018-03-05 23:24:55 +0100

    Create the a_thread_can_make_a_string_path() test.

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -32,6 +32,15 @@ class ThreadTest extends TestCase
     }
 
     /** @test */
+    public function a_thread_can_make_a_string_path()
+    {
+        $this->assertEquals(
+            "/threads/{$this->thread->channel->slug}/{$this->thread->id}",
+            $this->thread->path()
+        );
+    }
+
+    /** @test */
     public function a_thread_can_add_a_reply()
     {
         $this->thread->replies()->create([
