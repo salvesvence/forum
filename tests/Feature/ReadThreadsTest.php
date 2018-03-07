bf1e0bb21685c4a3eb3da1d46930d2665610adba
salvesvence <salvesvence@gmail.com>
2018-03-07 17:50:02 +0100

    Created the ChannelTest with the a_channel_consists_of_threads() test.

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -41,11 +41,11 @@ class ReadThreadsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_filter_threads_according_to_a_tag()
+    public function a_user_can_filter_threads_according_to_a_channel()
     {
         $channel = create('App\Channel');
-        $threadInChannel = create('App\Channel', ['channel_id' => $channel->id]);
-        $threadNotInChannel = create('App\Channel');
+        $threadInChannel = create('App\Thread', ['channel_id' => $channel->id]);
+        $threadNotInChannel = create('App\Thread');
 
         $this->get('/threads/' . $channel->slug)
              ->assertSee($threadInChannel->title)
