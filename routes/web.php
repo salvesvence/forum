065aca86feb9851bc43bd0c8feab2eebe15f29b4
salvesvence <salvesvence@gmail.com>
2018-03-07 17:31:53 +0100

    Added the channel filter to the index() function of ThreadsController class.

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('threads', 'ThreadsController@index')->name('threads.index');
-Route::get('threads/create', 'ThreadsController@create')->name('threads.create');
-Route::get('threads/{channel}/{thread}', 'ThreadsController@show')->name('threads.show');
-Route::post('threads', 'ThreadsController@store')->name('threads.store');
-
-Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store')
-    ->name('thread.replies.store');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('threads', 'ThreadsController@index')->name('threads.index');
+Route::get('threads/{channel}', 'ThreadsController@index')->name('threads.index');
+Route::get('threads/create', 'ThreadsController@create')->name('threads.create');
+Route::get('threads/{channel}/{thread}', 'ThreadsController@show')->name('threads.show');
+Route::post('threads', 'ThreadsController@store')->name('threads.store');
+Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store')
+    ->name('thread.replies.store');
+
+
