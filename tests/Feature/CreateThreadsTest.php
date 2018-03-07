60bb201786f0da7fcfcae0096c20398a391b957c
salvesvence <salvesvence@gmail.com>
2018-03-07 01:14:17 +0100

    Added the validation request to the store() function of ThreadsController and created the correspondent tests.

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -14,11 +14,11 @@ class CreateThreadsTest extends TestCase
     {
         $this->actingAs(create('App\User'));
 
-        $thread = create('App\Thread');
+        $thread = make('App\Thread');
 
-        $this->post(route('threads.store'), $thread->toArray());
+        $response = $this->post(route('threads.store'), $thread->toArray());
 
-        $this->get($thread->path())
+        $this->get($response->headers->get('Location'))
              ->assertSee($thread->title)
              ->assertSee($thread->body);
     }
@@ -41,5 +41,46 @@ class CreateThreadsTest extends TestCase
         $this->withExceptionHandling()
              ->get(route('threads.create'))
              ->assertRedirect('/login');
+    }
+
+    /** @test */
+    function a_thread_requires_a_title()
+    {
+        $this->publishThread(['title' => null])
+             ->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    function a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel', 2)->create();
+
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+    /**
+     * Publish a thread with the data given.
+     *
+     * @param array $overrides
+     * @return \Illuminate\Foundation\Testing\TestResponse
+     */
+    private function publishThread($overrides = [])
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = make('App\Thread', $overrides);
+
+        return $this->post('threads', $thread->toArray());
     }
 }
