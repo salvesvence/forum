1ab93b44dd214d0a684144a8e76f2841587d945c
salvesvence <salvesvence@gmail.com>
2018-03-08 03:01:27 +0100

    Now we shows the errors of validation forms.

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -10,17 +10,41 @@
                     <div class="panel-body">
                         <form method="post" action="{{ route('threads.store') }}">
                             {{ csrf_field() }}
+
+                            <div class="form-group">
+                                <label for="channel">Chose a channel:</label>
+                                <select name="channel_id" id="channel" class="form-control" required>
+                                    <option>Chose one...</option>
+                                    @foreach(\App\Channel::all() as $channel)
+                                        <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ ucfirst($channel->name) }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
                             <div class="form-group">
                                 <label for="title">Title:</label>
-                                <input type="text" class="form-control" id="title"
+                                <input type="text" class="form-control" id="title" required
                                        name="title" placeholder="title" value="{{ old('title') }}">
                             </div>
+
                             <div class="form-group">
                                 <label for="body">Body:</label>
-                                <textarea class="form-control" id="body" name="body"
-                                          placeholder="body" rows="8">value="{{ old('body') }}"</textarea>
+                                <textarea class="form-control" id="body" name="body" required
+                                          placeholder="body" rows="8">{{ old('body') }}</textarea>
                             </div>
-                            <button type="submit" class="btn btn-primary">Publish</button>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Publish</button>
+                            </div>
+
+                            @if(count($errors))
+                                <ul class="alert alert-danger">
+                                    @foreach($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            @endif
+
                         </form>
                     </div>
                 </div>
