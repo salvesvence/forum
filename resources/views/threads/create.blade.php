344d0c703a61341db2c33344bffddad5ff5f5f8a
salvesvence <salvesvence@gmail.com>
2018-03-08 02:42:51 +0100

    Added the old() values to the threads/create view.

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -13,12 +13,12 @@
                             <div class="form-group">
                                 <label for="title">Title:</label>
                                 <input type="text" class="form-control" id="title"
-                                       name="title" placeholder="title">
+                                       name="title" placeholder="title" value="{{ old('title') }}">
                             </div>
                             <div class="form-group">
                                 <label for="body">Body:</label>
                                 <textarea class="form-control" id="body" name="body"
-                                          placeholder="body" rows="8"></textarea>
+                                          placeholder="body" rows="8">value="{{ old('body') }}"</textarea>
                             </div>
                             <button type="submit" class="btn btn-primary">Publish</button>
                         </form>
