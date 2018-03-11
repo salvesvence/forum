b3529c959ba5b0b4e8c08d9c8384eba679aca587
salvesvence <salvesvence@gmail.com>
2018-03-11 22:42:40 +0100

    Added the initial content to the profile section.

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,5 @@ Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store')
     ->name('thread.replies.store');
 
 Route::post('replies/{reply}/favorites', 'FavoritesController@store')->name('favorites.store');
-Route::get('profiles/{user?}', 'ProfilesController@show');
+Route::get('profiles/{user?}', 'ProfilesController@show')->name('profile');
 
