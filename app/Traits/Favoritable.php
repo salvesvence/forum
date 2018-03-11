a2e91e5aecc6dc283edfbf43e3ab241af5215cd6
salvesvence <salvesvence@gmail.com>
2018-03-11 21:31:53 +0100

    Created the Favoritable trait.

---
+++ app/Traits/Favoritable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Traits;
+
+use App\Favorite;
+
+trait Favoritable
+{
+    /**
+     * Get all favorite models.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    /**
+     * Store a new favorite model.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        if(! $this->favorites()->where($attributes)->exists()) {
+            return $this->favorites()->create($attributes);
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFavorited()
+    {
+        return !! $this->favorites->where('user_id', auth()->id())->count();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
+    }
+}
