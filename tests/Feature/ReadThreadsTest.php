66bdc690f20db64c47b9a5c974be2470269da1a2
salvesvence <salvesvence@gmail.com>
2018-03-11 18:22:01 +0100

    Created the a_user_can_filter_threads_by_popularity() test.

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -64,4 +64,20 @@ class ReadThreadsTest extends TestCase
              ->assertSee($threadByJohn->title)
              ->assertDontSee($threadNotByJohn->title);
     }
+
+    /** @test */
+    public function a_user_can_filter_threads_by_popularity()
+    {
+        $threadsWithTwoReplies = create('App\Thread');
+        create('App\Reply', ['thread_id' => $threadsWithTwoReplies->id], 2);
+
+        $threadsWithThreeReplies = create('App\Thread');
+        create('App\Reply', ['thread_id' => $threadsWithThreeReplies->id], 3);
+
+        $threadsWithNoReplies = $this->thread;
+
+        $response = $this->getJson('threads?popular=1')->json();
+
+        $this->assertEquals([3, 2, 0], array_column($response, 'replies_count'));
+    }
 }
