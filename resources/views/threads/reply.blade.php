767619f2183a519394aed9f6f70b1a2bb547c0eb
salvesvence <salvesvence@gmail.com>
2018-03-17 21:15:58 +0100

    Now if the user is not logged cannot favorite the reply.

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -8,9 +8,11 @@
                         {{ $reply->owner->name }}
                     </a> said {{ $reply->created_at->diffForHumans() }}...
                 </h5>
-                <div>
-                    <favorite :reply="{{ $reply }}"></favorite>
-                </div>
+                @if(auth()->check())
+                    <div>
+                        <favorite :reply="{{ $reply }}"></favorite>
+                    </div>
+                @endif
             </div>
         </div>
 
