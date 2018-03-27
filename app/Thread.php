fb55f51895709032d40ac1faaaf27a0c4ce3c783
salvesvence <salvesvence@gmail.com>
2018-03-27 21:03:23 +0200

    Created the hasUpdateFor() function into the Thread model class.

--- app/Thread.php
+++ app/Thread.php
@@ -172,4 +172,16 @@ class Thread extends Model
             ->where('user_id', auth()->id())
             ->exists();
     }
+
+    /**
+     * Check if the current thread has been some update before the last cache record.
+     *
+     * @return bool
+     */
+    public function hasUpdatesFor()
+    {
+        $key = sprintf("users.%s.visits.%s", auth()->id(), $this->id);
+
+        return $this->updated_at > cache($key);
+    }
 }
