5adab8d2de35aecd577f4876cdef236d61f4b1c0
salvesvence <salvesvence@gmail.com>
2018-04-23 23:33:26 +0200

    Added the autocomplete functionality to the mentioned users section.

--- resources/assets/js/components/NewReply.vue
+++ resources/assets/js/components/NewReply.vue
@@ -2,22 +2,31 @@
     <div>
         <div v-if="signedIn">
             <div class="form-group">
-                <textarea id="body"
-                          name="body"
+                <textarea name="body"
+                          id="body"
                           class="form-control"
                           placeholder="Have something to say?"
                           rows="5"
                           required
-                          v-model="body">
-                </textarea>
+                          v-model="body"></textarea>
             </div>
-            <button type="submit" class="btn btn-default" @click="addReply">Post</button>
+
+            <button type="submit"
+                    class="btn btn-default"
+                    @click="addReply">Post</button>
         </div>
-        <p class="text-center" v-else>Please <a href="/login">sign in</a> to participate in this discussion.</p>
+
+        <p class="text-center" v-else>
+            Please <a href="/login">sign in</a> to participate in this
+            discussion.
+        </p>
     </div>
 </template>
 
 <script>
+    import 'jquery.caret';
+    import 'at.js';
+
     export default {
         data() {
             return {
@@ -29,6 +38,20 @@
             signedIn() {
                 return window.signedIn;
             }
+        },
+
+        mounted() {
+            $('#body').atwho({
+                at: "@",
+                delay: 750,
+                callbacks: {
+                    remoteFilter: function(query, callback) {
+                        $.getJSON("/api/users", {name: query}, function(usernames) {
+                            callback(usernames)
+                        });
+                    }
+                }
+            });
         },
 
         methods: {
