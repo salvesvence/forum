f7b532a64422437949c3ebc981706aef10d72fd0
salvesvence <salvesvence@gmail.com>
2018-04-23 19:40:55 +0200

    Refactored the NotifyMentionedUsers listener class.

--- app/Listeners/NotifyMentionedUsers.php
+++ app/Listeners/NotifyMentionedUsers.php
@@ -18,14 +18,13 @@ class NotifyMentionedUsers
      */
     public function handle(ThreadReceivedNewReply $event)
     {
-        $mentionedUsers = $event->reply->mentionedUsers();
-
-        foreach ($mentionedUsers as $name) {
-            $user = User::whereName($name)->first();
-
-            if($user) {
+        collect($event->reply->mentionedUsers())
+            ->map(function ($name) {
+                return User::whereName($name)->first();
+            })
+            ->filter()
+            ->each(function ($user) use($event) {
                 $user->notify(new YouWereMentioned($event->reply));
-            }
-        }
+            });
     }
 }
